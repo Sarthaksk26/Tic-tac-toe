996990407905249380e312fff4222d491cdeff86
Sarthaksk26 <sarthakkumbhar11@gmail.com>
2025-12-07 18:31:21 +0530

    Fix formatting issues in package.json and update vite.config.js for asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// vite.config.js or vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Set the base path to your repository name
+const GITHUB_REPO_NAME = '/Tic-tac-toe/';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: GITHUB_REPO_NAME, // <--- This fixes the asset paths
+});
